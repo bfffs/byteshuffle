1e72d0e6643592a4a9c2929728cb62713d3fd086
Alan Somers <asomers@gmail.com>
2025-11-28 12:46:27 -0700

    Make unshuffle unsafe
    It should've been unsafe all along.  Also, add some general
    documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! SIMD-accelerated byte shuffle/unshuffle routines
+//!
+//! The byte-shuffle is a very efficient way to improve the compressibility of data that consists
+//! of an array of fixed-size objects.  It rearranges the array in order to group all elements'
+//! least significant bytes together, most-significant bytes together, and everything in between.
+//! Since real applications' arrays often contain consecutive elements that are closely correlated
+//! with each other, this filter frequently results in lengthy continuous runs of identical bytes.
+//! Such runs are highly compressible by general-purpose compression libraries like gzip, lz4, etc.
+//!
+//! The [blosc](https://www.blosc.org) project was the original inspiration for this library.
+//! Blosc is a C library intended primarily for HPC users, and it implements a shuffle filter,
+//! among many other things.  This crate is a clean reimplementation of Blosc's shuffle filter.
+//!
+//! # Examples
+//!
+//! Typical use: a byte array consists of an arithmetic sequence.  Shuffle it, compress it,
+//! decompress it, and then unshuffle it.
+//! ```
+//! # use shuffle::*;
+//! const IN: [u8; 8] = [0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04];
+//! let mut shuffled = [0u8; 8];
+//! shuffle_bytes(2, &IN, &mut shuffled);
+//! assert_ne!(IN, shuffled);
+//! // In normal use, you would now serialize `shuffled`.  Then compress it, and later decompress
+//! // and deserialize it.  Then unshuffle like below.
+//! let mut unshuffled = [0u8; 8];
+//! unshuffle_bytes(2, &shuffled, &mut unshuffled);
+//! assert_eq!(IN, unshuffled);
+//! ```
 use std::{mem, str::FromStr};
 
 use cfg_if::cfg_if;
@@ -88,9 +117,24 @@ pub unsafe fn select_implementation(impl_: SimdImpl) {
 }
 
 /// Shuffle an array of fixed-size objects.
-pub fn shuffle<T: Copy>(src: &[T], dst: &mut [T]) {
-    assert_eq!(src.len(), dst.len());
+///
+/// The result will be a shuffled byte array.  Be aware that this byte array will usually not be
+/// portable.  It can only be unshuffled by the same computer architecture as the one that shuffled
+/// it, and sometimes only by the same compiler version.  If portability is desired, then first
+/// serialize the data and then use [`shuffle_bytes`].
+///
+/// # Examples
+/// ```
+/// # use shuffle::*;
+/// const IN: [u16; 4] = [0x01, 0x02, 0x03, 0x04];
+/// let mut out = [0u8; 8];
+/// shuffle(&IN, &mut out);
+#[cfg_attr(target_endian = "little", doc = "assert_eq!(out, [0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00, 0x00]);")]
+#[cfg_attr(target_endian = "big", doc = "assert_eq!(out, [0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04]);")]
+/// ```
+pub fn shuffle<T: Copy>(src: &[T], dst: &mut [u8]) {
     let ts = mem::size_of::<T>();
+    assert_eq!(src.len() * ts, dst.len());
     assert!(ts > 1, "No point shuffling plain [u8]");
     // Safe because of the first assertion.
     unsafe {
@@ -105,6 +149,15 @@ pub fn shuffle<T: Copy>(src: &[T], dst: &mut [T]) {
 
 /// Shuffle a byte array whose contents are known to approximately repeat with
 /// period `typesize`.
+///
+/// # Examples
+/// ```
+/// # use shuffle::*;
+/// const IN: [u8; 8] = [0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04];
+/// let mut out = [0u8; 8];
+/// shuffle_bytes(2, &IN, &mut out);
+/// assert_eq!(out, [0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04]);
+/// ```
 pub fn shuffle_bytes(typesize: usize, src: &[u8], dst: &mut [u8]) {
     assert_eq!(src.len(), dst.len());
     // Safe because of the first assertion.
@@ -119,21 +172,50 @@ pub fn shuffle_bytes(typesize: usize, src: &[u8], dst: &mut [u8]) {
 }
 
 /// Unshuffle an array of fixed-size objects.
-pub fn unshuffle<T: Copy>(src: &[T], dst: &mut [T]) {
-    assert_eq!(src.len(), dst.len());
+///
+/// # Safety
+///
+/// The `src` must've originally been produced by [`shuffle`], by a program that uses the exact
+/// same byte layout as this one.  That means the same wordsize, same endianness, and may even
+/// require the same compiler version.  If you can't guarantee those conditions, then serialize and
+/// use [`shuffle_bytes`]/[`unshuffle_bytes`] instead.
+///
+/// # Examples
+/// ```
+/// # use shuffle::*;
+/// const IN: [u16; 4] = [0x01, 0x02, 0x03, 0x04];
+/// let mut shuffled = [0u8; 8];
+/// shuffle(&IN, &mut shuffled);
+/// let mut out = [0u16; 4];
+/// unsafe { unshuffle(&shuffled, &mut out) };
+/// assert_eq!(IN, out);
+/// ```
+pub unsafe fn unshuffle<T: Copy>(src: &[u8], dst: &mut [T]) {
     let ts = mem::size_of::<T>();
+    assert_eq!(src.len(), dst.len() * ts);
     assert!(ts > 1, "No point shuffling plain [u8]");
     // Safe because of the first assertion.
     unsafe {
         generic::unshuffle(
             ts,
-            src.len() * ts,
+            dst.len() * ts,
             src.as_ptr() as *const u8,
             dst.as_ptr() as *mut u8,
         );
     }
 }
 
+/// Unshuffle a byte array whose contents are known to approximately repeat with
+/// period `typesize`.
+///
+/// # Examples
+/// ```
+/// # use shuffle::*;
+/// const IN: [u8; 8] = [0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04];
+/// let mut out = [0u8; 8];
+/// unshuffle_bytes(2, &IN, &mut out);
+/// assert_eq!(out, [0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04]);
+/// ```
 pub fn unshuffle_bytes(typesize: usize, src: &[u8], dst: &mut [u8]) {
     assert_eq!(src.len(), dst.len());
     unsafe {
@@ -158,18 +240,34 @@ mod t {
         #[test]
         fn twobytwo() {
             let src = [0x1234u16, 0x5678u16];
-            let mut dst = [0u16; 2];
+            let mut dst = [0u8; 4];
             shuffle(&src[..], &mut dst[..]);
-            assert_eq!(dst, &[0x7834u16, 0x5612][..]);
+            cfg_if! {
+                if #[cfg(target_endian = "big")] {
+                    let expected = [0x78u8, 0x34, 0x56, 0x12];
+                } else {
+                    let expected = [0x34, 0x78u8, 0x12, 0x56];
+                }
+            }
+            assert_eq!(dst, &expected[..]);
         }
 
         // Four elements of four bytes each
         #[test]
         fn fourbyfour() {
             let src = [0x11223344u32, 0x55667788, 0x99aabbcc, 0xddeeff00];
-            let mut dst = [0u32; 4];
+            let mut dst = [0u8; 16];
             shuffle(&src[..], &mut dst[..]);
-            assert_eq!(dst, &[0x00cc8844, 0xffbb7733, 0xeeaa6622, 0xdd995511][..]);
+            cfg_if! {
+                if #[cfg(target_endian = "big")] {
+                    let expected = [0x00u8, 0xcc, 0x88, 0x44, 0xff, 0xbb, 0x77, 0x33,
+                       0xee, 0xaa, 0x66, 0x22, 0xdd, 0x99, 0x55, 0x11];
+                } else {
+                    let expected = [0x44, 0x88, 0xcc, 0x00, 0x33, 0x77, 0xbb, 0xff,
+                       0x22, 0x66, 0xaa, 0xee, 0x11, 0x55, 0x99, 0xdd];
+                }
+            }
+            assert_eq!(dst, &expected[..]);
         }
     }
 
@@ -215,18 +313,32 @@ mod t {
         // Two elements of two bytes each
         #[test]
         fn twobytwo() {
-            let src = [0x7834u16, 0x5612];
+            cfg_if! {
+                if #[cfg(target_endian = "big")] {
+                    let src = [0x78u8, 0x34, 0x56, 0x12];
+                } else {
+                    let src = [0x34, 0x78u8, 0x12, 0x56];
+                }
+            }
             let mut dst = [0u16; 2];
-            unshuffle(&src[..], &mut dst[..]);
+            unsafe{ unshuffle(&src[..], &mut dst[..]) };
             assert_eq!(dst, &[0x1234u16, 0x5678u16][..]);
         }
 
         // Four elements of four bytes each
         #[test]
         fn fourbyfour() {
-            let src = [0x00cc8844, 0xffbb7733, 0xeeaa6622, 0xdd995511];
+            cfg_if! {
+                if #[cfg(target_endian = "big")] {
+                    let src = [0x00u8, 0xcc, 0x88, 0x44, 0xff, 0xbb, 0x77, 0x33,
+                       0xee, 0xaa, 0x66, 0x22, 0xdd, 0x99, 0x55, 0x11];
+                } else {
+                    let src = [0x44, 0x88, 0xcc, 0x00, 0x33, 0x77, 0xbb, 0xff,
+                       0x22, 0x66, 0xaa, 0xee, 0x11, 0x55, 0x99, 0xdd];
+                }
+            }
             let mut dst = [0u32; 4];
-            unshuffle(&src[..], &mut dst[..]);
+            unsafe{ unshuffle(&src[..], &mut dst[..]) };
             assert_eq!(
                 dst,
                 &[0x11223344u32, 0x55667788, 0x99aabbcc, 0xddeeff00][..]
